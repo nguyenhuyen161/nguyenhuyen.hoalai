6544e0e727c4bc709d058e2612588781df701735
nguyenhuyen161 <107852502+nguyenhuyen161@users.noreply.github.com>
2022-12-01 13:53:50 +0900

    Add files via upload

---
+++ 01_omikuji3/js/omikuji.js
@@ -0,0 +1,100 @@
+"use strict";
+let n;
+let nBefore;
+window.addEventListener("DOMContentLoaded",
+function(){
+    $("header").textillate({
+        loop: false, 
+        minDisplayTime: 2000, 
+        initialDelay: 2000, 
+        autoStart: true, 
+        in: { 
+        effect: "fadeInLeftBig", 
+        delayScale: 1.5, 
+        delay: 50, 
+        sync: false, 
+        shuffle: true 
+        }
+        });
+        
+        $(function(){
+        ScrollReveal().reveal("#btn1", { duration: 9000 });
+        });
+        setTimeout(
+            function(){
+                let popmsg="いらっしゃい!おみくじ引いてって!";
+                window.alert(popmsg);
+        },
+        "5000"
+        );
+},false
+);
+let soundEndflag ="0";
+const btn1= document.getElementById("btn1");
+const omikujiText=document.getElementById("omikujiText");
+const omikujiTextImage=document.getElementById("omikujiTextImage")
+btn1.addEventListener("click",
+function(){
+    if(soundEndflag === "1"){
+        soundControl("end","");
+    }
+    /*let resultText =[ "大吉!!!!!","吉!!!!","中吉!!!","小吉!!","末吉!","凶.."];
+    let resultColor =["#ff0000","#c71585","#ff1493","#ff69b4","#ff8c00","#1e90ff"];
+    let resultFontSize =["90px","80px","70px","60px","50px","40px"];*/
+    let resultText =[ "img/daikichi.png","img/chukichi.png","img/syokichi.png","img/suekichi.png","img/daikyo.png"];
+    let resultSound =["sound/omikuji_sound1.mp3","sound/omikuji_sound2.mp3",
+    "sound/omikuji_sound3.mp3","sound/omikuji_sound4.mp3","sound/omikuji_sound5.mp3"];
+    //let n=Math.floor(Math.random()*resultText.length);
+    while(n==nBefore){
+        n=Math.floor(Math.random()*resultText.length);
+    }
+    nBefore=n;
+    let resultMaxSpeed =[10,10,8,5,5];
+    let resultMaxSize=[30,30,30,40,30];
+    let resultImage=["img/star.png","img/sakura_hanabira.png",
+    "img/water1.png","img/redLeaves4.png","img/snowflakes.png"];
+    
+    /*omikujiText.textContent=resultText[n];
+    omikujiText.style.color=resultColor[n];
+    omikujiText.style.fontSize=resultFontSize[n];*/
+    omikujiTextImage.src=resultText[n];
+    omikujiTextImage.classList.add("omikujiPaper");
+    omikujiTextImage.addEventListener("animationend",
+       function(){
+        omikujiTextImage.classList.remove("omikujiPaper");
+    }, false                               
+    );
+    w_sound=resultSound[n];
+    soundControl("start",w_sound);
+    soundEndflag="1";
+    $(document).snowfall("clear");
+    setTimeout(
+      function(){
+        $(document).ready(function(){
+          $(document).snowfall({
+            
+            maxSpeed : resultMaxSpeed[n],
+            minSpeed : 1,
+            maxSize : resultMaxSize[n],
+            minSize : 1,
+            image : resultImage[n],
+        });
+    });
+      },
+        "200"
+    );
+},false
+);
+let w_sound 
+let music
+function soundControl(status, w_sound){
+    if(status==="start"){
+        music=new Audio(w_sound);
+        music.currentTime=0;
+        music.play();
+    } else if(status==="end"){
+        music.pause();
+        music.currentTime=0;
+    }
+}
+
